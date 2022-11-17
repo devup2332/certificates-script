5646b5a6b045f03f922e0d6f06cd12a492738ecd
diego rojas <devup2332@gmail.com>
2022-11-17 17:08:46 -0500

    d

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const dd: any[] = []
 let coursesToDownload: any[] = []
 
 const clientId = "universidadchinoin";
-let index = 0;
+let index = 0; //42
 
 const downloadCertificates = async () => {
   const { user_course_cl } = await client.request(
@@ -90,52 +90,91 @@ const downloadCertificates = async () => {
   }
 };
 
-const downloadCertsErrors = async (data: any[]) => {
-  const errorsFile = path.resolve(__dirname, "../src") + '/errors.json'
-  fs.readFile(errorsFile, 'utf-8', (err, data) => {
-    if (err) {
-      console.log({ err })
-    } else {
-      errors = JSON.parse(data)
-    }
+const sleep = (time: number) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(resolve, time)
   })
-  index = 0;
-  while (index <= data.length - 1) {
-    const c = coursesToDownload[errors[index]];
-    const params = new URLSearchParams({
-      clientId,
-      userName: c.user?.full_name || "",
-      courseName: c.course?.name || "",
-    });
-    const { data } = await axios.get(
-      `${environments.CERT_SERVER_URL}/${environments.CERT_SERVER_ENDPOINT}?${params}`
-    );
-    const certUrl = `${environments.CERT_SERVER_URL}/${data}`;
+}
 
-    const response = await axios.get(certUrl, {
-      responseType: "arraybuffer",
-    });
-    const dir = path.resolve(__dirname, "../certificates/certificates1/");
-    const filename = `${c.user.full_name.replace(
-      " ",
-      "-"
-    )}-${c.course.name.replace(" ", "-")}`;
-    fs.writeFile(dir + filename, response.data, () => {
-      console.log("Download finished" + index);
-      index++;
-    });
+const downloadCertsErrors = async (errors: number[]) => {
+  const { user_course_cl } = await client.request(
+    GET_APPROVED_USERS_BY_CLIENTID,
+    {
+      clientId,
+    }
+  );
+  const { user_course_cl: marketplace_courses } = await client.request(
+    GET_APPROVED_USERS_IN_MARKETPLACE,
+    {
+      clientId,
+    }
+  );
+  const mkApproved = marketplace_courses.filter((c: any) => {
+    const approved =
+      c.score >= c.course.min_score && c.progress >= c.course.min_progress;
+    if (c.completed_at && approved) return true;
+    return false;
+  });
+  const response = user_course_cl.filter((c: any) => {
+    const approved =
+      c.score >= c.course.min_score && c.progress >= c.course.min_progress;
+    if (c.completed_at && approved) return true;
+    return false;
+  });
+  const dc3 = response.filter((c: any) => {
+    return c.course.dc3_data_json;
+  });
+
+  console.log({ l: mkApproved.length, l2: response.length, l3: dc3.length });
+
+  const certsToDownload = [...response, ...mkApproved];
+  while (index <= errors.length - 1) {
+    try {
+
+      await sleep(500)
+      const c = certsToDownload[errors[index]];
+      const params = new URLSearchParams({
+        clientId,
+        userName: c.user?.full_name.trimStart().trimEnd().replace(/\s/g, "-") || "",
+        courseName: c.course?.name.trimStart().trimEnd().replace(/\s/g, '-') || "",
+      });
+      const { data } = await axios.get(
+        `${environments.CERT_SERVER_URL}/${environments.CERT_SERVER_ENDPOINT}?${params}`
+      );
+      const certUrl = `${environments.CERT_SERVER_URL}/${data}`;
+
+      const response = await axios.get(certUrl, {
+        responseType: "arraybuffer",
+      });
+      const dir = path.resolve(__dirname, "../certificatesChinoin");
+      const filename = `/${c.user.full_name.replace(
+        /\s/g,
+        "-"
+      )}-${c.course.name.replace(" ", "-")}`;
+      fs.writeFile(dir + filename, response.data, () => {
+        console.log("Download finished error: " + errors[index] + ' - ' + index);
+        index++;
+      });
+    } catch (err) {
+      // console.log(err)
+      console.log('Error', {
+        name: certsToDownload[errors[index]].user.full_name,
+        course: certsToDownload[errors[index]].course.name
+      })
+      index++
+    }
   }
 };
 
-downloadCertificates().then(() => {
-  console.log({ errors });
-  const dir = path.resolve(__dirname, "../src");
-  const filename = '/errors.json'
-  fs.writeFile(dir + filename, JSON.stringify(errors), 'utf-8', () => {
-    console.log('Json wrote')
-    downloadCertsErrors(errors);
-  })
-});
+// downloadCertificates().then(() => {
+//   console.log({ errors });
+//   const dir = path.resolve(__dirname, "../src");
+//   const filename = '/errors.json'
+//   fs.writeFile(dir + filename, JSON.stringify(errors), 'utf-8', () => {
+//     console.log('Json wrote')
+//     downloadCertsErrors(errors);
+//   })
+// });
 
 // const example = [1, 2, 3, 5, 6, 7, 8, 3, 5, 1, 2, 8, 6, 22, 12, 33]
 // const dir = path.resolve(__dirname, "../src/");
@@ -144,3 +183,17 @@ downloadCertificates().then(() => {
 // fs.writeFile(dir + filename, JSON.stringify(example), 'utf-8', () => {
 //   console.log('example wrote')
 // })
+
+
+
+const dir = path.resolve(__dirname, "../src");
+const filename = '/errors.json'
+fs.readFile(dir + filename, 'utf-8', (err, data) => {
+  if (err) {
+    console.log({ err })
+  } else {
+    const response = JSON.parse(data)
+    console.log({ response })
+    downloadCertsErrors(response)
+  }
+})
